53e416ed6a2c5c18dd869f9c3dca12be33aaeac9
Lingzi <susanzhang0521@gmail.com>
2023-09-29 17:40:19 -0400

    created basic form ui

--- client/src/components/FeedbackForm.js
+++ client/src/components/FeedbackForm.js
@@ -1,6 +1,15 @@
-// Render Prop
+import {
+  Box,
+  Button,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+  Rating
+} from "@mui/material";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { useState } from "react";
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 import '../css/FeedbackForm.css'
 
 // rate experience 
@@ -8,42 +17,108 @@ import '../css/FeedbackForm.css'
 // what did you not like 
 // any suggestions
 
-const FeedbackForm = () => (
-  <div>
-    <h1>Any place in your app!</h1>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+const FeedbackForm = () => {
+  const initialValue = {
+    rate: 0,
+    like: "",
+    notLike: "",
+    suggestions: "",
+  };
+
+  const handleSubmit = (values, props) => {
+    console.log(values);
+    alert(JSON.stringify(values));
+
+    props.resetForm();
+  };
+
+  return (
+    <Grid container>
+      <Grid item sm={3} xs={false}></Grid>
+      <Grid item sm={6} xs={12}>
+        <Paper>
+          <Box m={5} p={3}>
+            <Typography variant="h5">Basic Formik Form Validation</Typography>
+            <Formik
+              initialValues={initialValue}
+              // validationSchema={YupValidation}
+              onSubmit={handleSubmit}
+            >
+              {(props) => {
+                const { rate, like, notLike, suggestions } = props.values;
+                return (
+                  <Form>
+                    {/* First Way */}
+                    <Rating 
+                      label="rating"
+                      name="half-rating"
+                      defaultValue={rate}
+                      precision={0.5}
+                      required
+                    />
+                    <TextField
+                      label="What did you like about Better Grocery?"
+                      name="like"
+                      fullWidth
+                      variant="outlined"
+                      margin="dense"
+                      value={like}
+                      onChange={props.handleChange}
+                      onBlur={props.handleBlur}
+                    />
+                    {/* Second Way */}
+                    <Field
+                      as={TextField}
+                      label="What did you not like about Better Grocery?"
+                      type="notLike"
+                      name="notLike"
+                      fullWidth
+                      variant="outlined"
+                      margin="dense"
+                    />
+
+                    <Field
+                      as={TextField}
+                      label="Any other suggestions?"
+                      name="suggestions"
+                      fullWidth
+                      variant="outlined"
+                      margin="dense"
+                    />
+
+                    <Button
+                      variant="contained"
+                      type="submit"
+                      color="primary"
+                      fullWidth
+                    >
+                      Submit
+                    </Button>
+                  </Form>
+                );
+              }}
+            </Formik>
+          </Box>
+        </Paper>
+      </Grid>
+      <Grid item sm={3} xs={false}></Grid>
+    </Grid>
+  )
+}
+  // <div className='form-box'>
+  //   <h2>Give us your feedback!</h2>
+  //   <form>
+  //     <div className = "feedback">
+  //       <label>What did you like about Better Grocery?</label>
+  //       <textarea />  
+  //       <label>What did you not like about Better Grocery?</label>      
+  //       <textarea />
+  //       <label>Any other suggestions?</label>
+  //       <textarea placeholder="suggestions"/>
+  //     </div>
+  //     <button type = "submit" id= "submitBtn" className = "submitBtn"> submit</button>
+  //   </form>
+  // </div>
+
 
 export default FeedbackForm;
