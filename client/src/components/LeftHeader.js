cc5e3648e2cab83196805452c2c3b884db5bb7d0
cjeong22 <chris_jeong@brown.edu>
2023-09-30 02:05:26 -0400

    quickly adding an image link to the home page

--- client/src/components/LeftHeader.js
+++ client/src/components/LeftHeader.js
@@ -4,7 +4,10 @@ import Logo from '../images/wellfareLogo.png'
 const LeftHeader = () => {
   return (
     <div className="leftHeader">
+      <a href = 'http://localhost:3000'>
         <img src = {Logo} alt = "Wellfare Logo" width = {150} height = {50}/>
+      </a>
+        
     </div>
   )
 }
