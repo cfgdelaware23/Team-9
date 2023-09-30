356c969908adf5adc82a69701425bfc111e946af
cjeong22 <chris_jeong@brown.edu>
2023-09-29 21:04:43 -0400

    fixed admin pages

---
+++ client/src/components/AdminCheckout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/AdminCheckout.css'
+
+
+const AdminCheckout = () => {
+    return (
+        <div className='admin-checkout'>
+            AdminCheckout
+        </div>
+    )
+}
+
+
+export default AdminCheckout
