e966ea7a70c3799c599088fc8df1abf9c385f263
cjeong22 <chris_jeong@brown.edu>
2023-09-29 23:25:00 -0400

    footer done

---
+++ client/src/components/Socials.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Socials = () => {
+  return (
+    <div className='socials'>
+      <b> Socials </b>
+      <a href = 'https://www.wellfare.org/'> 
+        <font size = '4'>
+          Main Site
+        </font>
+      </a>
+      <a href = 'https://www.linkedin.com/company/wellfareorganization/'>
+        <font size = '4'>
+          LinkedIn
+        </font>
+      </a>
+      <a href = 'https://www.instagram.com/wellfareorg'>
+        <font size = '4'>
+          Instagram
+        </font>
+      </a>
+    </div>
+  )
+}
+
+export default Socials
