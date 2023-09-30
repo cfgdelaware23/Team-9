37772e464f671717b2d5b0cba7ab41f94cfef912
nikhailmann <nikhailmann@gmail.com>
2023-09-29 20:43:38 -0400

    pre merge

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+from models.user import User
+from models.purchase import Purchase
+import hashlib
 import datetime
-from models.user import User  
-from models.purchase import Purchase  
 
 app = Flask(__name__)
 
@@ -10,51 +11,60 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mydatabase"
 mongo = PyMongo(app)
 
-# Routes
-@app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = mongo.db.users.find()
+    user_list = [user for user in users]
+    return jsonify(user_list)
+
+@app.route('/user/<id>', methods=['GET'])
+def get_user(id):
+    user = mongo.db.users.find_one({"membership_id": id})
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route('/user/<id>/purchases', methods=['GET'])
+def get_user_purchases(id):
+    user = mongo.db.users.find_one({"membership_id": id})
+    if user:
+        return jsonify(user.get('purchase_history', []))
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        snap_ebt_number = request.form['snap_ebt_number']
-        email = request.form.get('email')  
-        age = request.form.get('age')
-        household_size = request.form.get('household_size')
+    data = request.json
+    
+    # creates the hash from a user's first and last name, and address
+    unique_info = data['first_name'] + data['last_name'] + data['address']
+    user_hash = hashlib.sha256(unique_info.encode()).hexdigest()
 
-        user = User(first_name, last_name, snap_ebt_number, email, age, household_size)
+    # Check if user with the same hash already exists
+    existing_user = mongo.db.users.find_one({"user_hash": user_hash})
+    if existing_user:
+        return jsonify({"error": "User already exists"}), 400
 
-        # Example of adding a purchase to the user
-        # purchase_date = datetime.datetime.now()
-        # purchase = Purchase(user.membership_id, purchase_date, 20.5, "Item Name")
-        # user.add_purchase(purchase.__dict__)
+    user = User(data['first_name'], data['last_name'], data['address'], 
+                data.get('snap_ebt_number'), data.get('email'), data.get('age'), data.get('household_size'))
+    
+    user.set_user_hash(user_hash)
+    
+    purchase_date = datetime.datetime.now()
+    purchase = Purchase(user.membership_id, purchase_date, 20.5, "Item Name")
+    user.add_purchase(purchase.__dict__)
 
-        mongo.db.users.insert_one(user.__dict__)
+    mongo.db.users.insert_one(user.__dict__)
+    return jsonify({"message": "User added successfully", "membership_id": user.membership_id}), 201
 
-        return redirect(url_for('home'))
-
-# application form render
-@app.route('/application')
-def application():
-    return render_template('application.html')
-
-# cashier checkout page render
-@app.route('/checkout')
-def checkout():
-    return render_template('checkout.html')
-
-# admin (cashier) page render
-@app.route('/admin')
-def admin():
-    return render_template('admin.html')
-
-# main user page render
-@app.route('/user')
-def user():
-    return render_template('user.html')
+@app.route('/user/<id>/qualify', methods=['GET'])
+def check_qualify(id):
+    user = mongo.db.users.find_one({"membership_id": id})
+    if user:
+        return jsonify({"qualify_discount": user.get('qualify_discount', False)})
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/models/user.py
+++ server/models/user.py
@@ -1,20 +1,21 @@
-from pymongo.collection import ObjectId
+import uuid
 
 class User:
-    def __init__(self, first_name, last_name, snap_ebt_number, email=None, age=None, household_size=None):
-        self.membership_id = str(ObjectId())  # this will generate the unique ID
+    def __init__(self, first_name, last_name, address, snap_ebt_number=None, email=None, age=None, household_size=None):
+        self.membership_id = str(uuid.uuid4())  # Generate unique membership ID
         self.first_name = first_name
         self.last_name = last_name
-        self.snap_ebt_number = snap_ebt_number
-        self.email = email
-        self.age = age
-        self.household_size = household_size
-        self.qualify_discount = self.check_discount()  
+        self.address = address
+        self.snap_ebt_number = snap_ebt_number  
+        self.email = email  
+        self.age = age  
+        self.household_size = household_size  
+        self.qualify_discount = False  
         self.purchase_history = []
-
-    def check_discount(self):
-        # where we can incorporate the API
-        pass
+        self.user_hash = ""  
 
     def add_purchase(self, purchase):
         self.purchase_history.append(purchase)
+
+    def set_user_hash(self, user_hash):
+        self.user_hash = user_hash
